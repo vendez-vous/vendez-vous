e891394d667432ff17c4de929cb59e3155412f88
parth_shel <ps@parthshel.com>
2018-04-11 23:04:53 +0000

    installed express.js and nodemon

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const app = express();
 app.listen(5108, function() {
 		  console.log('listening on 5108')
 		  })
+
+app.get('/', function(req, res) {
+		  res.send('Hello World - vendez-vous')
+		  })
