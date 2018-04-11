a4ce19ea94653fb8d8b3cff38cc57cdba88f506f
parth_shel <ps@parthshel.com>
2018-04-11 23:13:19 +0000

    installed body parsing dependency

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 console.log('vendez-vous')
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(5108, function() {
 		  console.log('listening on 5108')
