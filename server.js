e79214d5435acc16cc99795d3dcf5e2c9f217359
parth_shel <ps@parthshel.com>
2018-04-12 03:42:47 +0000

    working on mongo-db

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3702, function() {
-		  console.log('vendez-vous server started')
-		  })
+var db
+
+MongoClient.connect('mongodb://api:cs252lab6@ds119618.mlab.com:19618/vendez-vous-users', (err, client) => {
+	if (err) return console.log(err)
+	db = client.db('vendez-vous-users') // whatever your database name is
+	app.listen(3702, () => {
+		console.log('vendez-vous server started')
+		})
+	})
 
 app.get('/', function(req, res) {
-		  res.send('Hello World - vendez-vous')
-		  })
+	res.send('Hello World - vendez-vous')
+	})
