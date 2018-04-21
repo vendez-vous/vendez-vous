4999cdce929bbee4fce69e2a67b38755692492b4
parth-shel <ps@parthshel.com>
2018-04-21 15:46:36 -0400

    working on location and nearby

--- server.js
+++ server.js
@@ -3,60 +3,181 @@
  *
  * @author: parth_shel
  * @version: v:1.0 Apr 11, 2018
-**/
+ **/
 
 console.log('vendez-vous server started')
 
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 var db
 
 MongoClient.connect('mongodb://api:cs252lab6@ds119618.mlab.com:19618/vendez-vous-users', (err, client) => {
-	if (err) return console.log(err)
-	db = client.db('vendez-vous-users')
-	console.log('connected to mongodb')
-	app.listen(3702, () => {
-		console.log('listening on 3702')
-		})
-	})
+  if (err) return console.log(err)
+  db = client.db('vendez-vous-users')
+  console.log('connected to mongodb')
+  app.listen(3702, () => {
+    console.log('listening on 3702')
+  })
+})
 
 app.get('/', function(req, res) {
-	res.send('vendez-vous api')
-	});
+  res.send('vendez-vous api')
+});
 
 app.post('/user-login', (req, res) => {
-	console.log(req);
-	var user = {
-		"_id": req.body.id,
-		"user_name": req.body.name,
-		"picture": req.picture.data.url,
-		"location": {},
-		"bio": "",
-		"interests": "",
-		"matches": []
-	};
-	// check if exists first
-	db.collection('users').findOne({"_id": user._id}, function (err, result) {
-			if(err) console.log(err)
-			else {
-				if (result) {
-					res.send("user exists!");
-					}
-				else {
-					// add to database
-					db.collection('users').save(user, (err, result) => {
-					if (err) console.log(err)
-					else {
-						console.log('saved to database')
-						res.send("user logged in!")
-						}
-					});
-				}
-			}
-		});
+  var user = {
+    "_id": req.body.id,
+    "user_name": req.body.name,
+    "picture": req.body.picture,
+    "location": {},
+    "bio": "",
+    "interests": "",
+    "matches": []
+  };
+  // check if exists
+  db.collection('users').findOne({
+    "_id": user._id
+  }, function(err, result) {
+    if (err) console.log(err);
+    else {
+      if (result) {
+        res.send("user exists!");
+        console.log("user exists in the database");
+      } else {
+        // add to database
+        db.collection('users').save(user, (err, result) => {
+          if (err) console.log(err);
+          else {
+            console.log('saved to database');
+            res.send("user logged in!");
+          }
+        });
+      }
+    }
+  });
 });
+
+app.post('/update-profile', (req, res) => {
+  var user = {
+    "_id": req.body.id,
+    "bio": req.body.bio,
+    "interests": req.body.interests
+  };
+  // check if exists
+  db.collection('users').findOne({
+    "_id": user._id
+  }, function(err, result) {
+    if (err) console.log(err);
+    else {
+      if (result) {
+        // update database
+        db.collection('users').update({
+          "_id": req.body.id
+        }, {
+          $set: {
+            "bio": user.bio,
+            "interests": user.interests
+          }
+        }, {
+          w: 1
+        }, (err, result) => {
+          if (err) console.log(err);
+          else {
+            console.log('updated database');
+            res.send("user profile updated");
+          }
+        });
+      } else {
+        res.send("user does not exist!");
+        console.log("user does not exist in the database");
+      }
+    }
+  });
+});
+
+app.post('/like', (req, res) => {
+  var user = {
+    "_id": req.body.id,
+    "other_user": req.body.other
+  };
+  // check if exists
+  db.collection('users').findOne({
+    "_id": user.other_user
+  }, function(err, result) {
+    if (err) console.log(err);
+    else {
+      if (result) {
+        // update database
+        var matches_copy = result.matches.slice()
+        matches_copy.push(user._id);
+        db.collection('users').update({
+          "_id": user.other_user
+        }, {
+          $set: {
+            // TODO append to the array of matches:
+            "matches": matches_copy
+          }
+        }, {
+          w: 1
+        }, (err, result) => {
+          if (err) console.log(err);
+          else {
+            console.log('updated database');
+            res.send("liked! ;)");
+          }
+        });
+      } else {
+        res.send("this user does not exist!");
+        console.log("user does not exist in the database");
+      }
+    }
+  });
+});
+
+app.get('/nearby', (req, res) => {
+  var user = {
+    "_id": req.body.id
+  };
+  // check if exists
+  db.collection('users').findOne({
+    "_id": user._id
+  }, function(err, result) {
+    if (err) console.log(err);
+    else {
+      if (result) {
+        // TODO get nearby users and if a user has liked current user
+      } else {
+        res.send("this user does not exist!");
+        console.log("user does not exist in the database");
+      }
+    }
+  });
+});
+
+function isNearby(user1, user2) {
+	if(distance(user1.location.latitude, user1.longitude, user2.latitude, user2.longitude) <= 5) { // 5 mile radius
+		return true
+	}
+	return false
+}
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	return dist
+}
